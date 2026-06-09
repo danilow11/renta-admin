69717e9d39adc364e9ea8e64acb2725f1194be78
daniel.alvarez <daniel.alvarez.m11@gmail.com>
2026-06-09 12:33:56 -0600

    chore: wire prisma client into api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../../.env',
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,8 +1,22 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(configService: ConfigService) {
+    const databaseUrl = configService.get<string>('DATABASE_URL');
+
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is required to initialize PrismaClient.');
+    }
+
+    const adapter = new PrismaPg({ connectionString: databaseUrl });
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
